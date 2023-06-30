26bf5c14d87d507437d6cdd71ebb8eed1922cc72
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-06-30 20:32:16 +0530

    icons refactor

---
+++ src/assets/images/icons/index.ts
@@ -0,0 +1,9 @@
+import Care from "./care.svg";
+import Just from "./social.svg";
+import Together from "./together.svg";
+import Eco from "./eco.svg";
+import Rights from "./rights.svg";
+import Participation from "./participation.svg";
+import Welfare from "./welfare.svg";
+
+export { Care, Just, Together, Eco, Rights, Participation, Welfare }
