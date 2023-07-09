7139d46c4a32b1387757528ce059d3a961185ef5
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-07-09 18:11:28 +0530

    changed colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			// colors: {
+			// 	'primary': '#FC5D35',
+			// 	'secondary': '#FFF1EF',
+			// 	'tertiary': '#B36F6B',
+			// },
 			colors: {
-				'primary': '#FC5D35',
-				'secondary': '#FFF1EF',
+				'primary': '#3db049',
+				'secondary': '#e3f2fc',
 				'tertiary': '#B36F6B',
 			}
 		},
