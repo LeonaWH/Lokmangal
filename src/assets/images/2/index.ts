fa21153f403edb0a5646d9019264067dc6a84b9e
Alston <0508alstonfernadnes@gmail.com>
2023-07-17 20:37:38 +0530

    Updated carousel on programs section

---
+++ src/assets/images/2/index.ts
@@ -0,0 +1,7 @@
+import TwoOne from "./1.jpg";
+import TwoTwo from "./2.jpg";
+import TwoThree from "./3.jpg";
+import TwoFour from "./4.jpg";
+import TwoFive from "./5.jpg";
+import TwoSix from "./6.jpg";
+export { TwoOne, TwoTwo, TwoThree, TwoFour, TwoFive, TwoSix }
---
+++ src/assets/images/4/index.ts
@@ -0,0 +1,8 @@
+import FourOne from "./1.jpg";
+import FourTwo from "./2.jpg";
+import FourThree from "./3.jpg";
+import FourFour from "./4.jpg";
+import FourFive from "./5.jpg";
+import FourSix from "./6.jpg";
+
+export { FourOne, FourTwo, FourThree, FourFour, FourFive, FourSix }
