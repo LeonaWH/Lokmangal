e0e18ad5ffca349fa4a01f6cc4c1359d792740e1
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-07-20 15:06:53 +0530

    removed index files from programs

--- src/assets/images/programs/1/index.ts
+++
@@ -1,8 +0,0 @@
-import OneOne from "./1.jpg";
-import OneTwo from "./2.jpg";
-import OneThree from "./3.jpg";
-import OneFour from "./4.jpg";
-import OneFive from "./5.jpg";
-import OneSix from "./6.jpg";
-import OneSeven from "./7.jpg";
-export { OneOne, OneTwo, OneThree, OneFour, OneFive, OneSix, OneSeven }
