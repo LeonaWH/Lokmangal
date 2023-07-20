3f3e5e0853109238447e98dee616d66cc83c4560
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-07-20 17:00:43 +0530

    donate top bar

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,12 +1,14 @@
 import ArrowLeft from "./arrow-left.svg";
 import ArrowRight from "./arrow-right.svg";
 import Care from "./care.svg";
-import Just from "./social.svg";
-import Together from "./together.svg";
 import Eco from "./eco.svg";
-import Rights from "./rights.svg";
-import Participation from "./participation.svg";
-import Welfare from "./welfare.svg";
 import Hamburger from "./hamburger.svg";
+import Just from "./social.svg";
+import Mail from "./mail.svg";
+import Participation from "./participation.svg";
+import Phone from "./phone.svg"
+import Rights from "./rights.svg";
+import Together from "./together.svg";
+import Welfare from "./welfare.svg";
 
-export { ArrowLeft, ArrowRight, Care, Just, Together, Eco, Rights, Participation, Welfare, Hamburger }
+export { ArrowLeft, ArrowRight, Care, Just, Mail, Together, Eco, Rights, Participation, Phone, Welfare, Hamburger }
