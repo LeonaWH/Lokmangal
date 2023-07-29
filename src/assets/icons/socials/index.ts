bd147fdf6737c1cea9f2ffea9444421bf29230f7
sd8capricon <sd8capricon@gmail.com>
2023-07-29 18:35:36 +0530

    contributors page

---
+++ src/assets/icons/socials/index.ts
@@ -0,0 +1,8 @@
+import facebook from './facebook.astro';
+import github from './github.astro';
+import instagram from './instagram.astro';
+import linkedin from './linkedin.astro';
+import twitter from './twitter.astro';
+import globe from './globe.astro';
+
+export default { facebook, github, instagram, linkedin, twitter, globe };
